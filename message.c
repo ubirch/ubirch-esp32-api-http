8b40097e81abd74465d8aa45d4d30b5d1883da28
Roxana Meixner <roxana.meixner@gmail.com>
2019-12-17 17:06:41 +0100

    [WIP] fix getting 503 response from backend

--- message.c
+++ message.c
@@ -55,11 +55,7 @@ esp_err_t ubirch_store_signature(unsigned char *signature, size_t len) {
     return err;
 }
 
-esp_err_t *ubirch_message(msgpack_sbuffer *sbuf, const unsigned char *uuid, int32_t *values, uint16_t num) {
-    // create and initialize ubirch protocol context and packer for msgpack type payload
-    ubirch_protocol *upp = ubirch_protocol_new(uuid, ed25519_sign);
-    msgpack_packer *pk = msgpack_packer_new(&sbuf, msgpack_sbuffer_write);
-
+esp_err_t *ubirch_message(ubirch_protocol *upp, int32_t *values, uint16_t num) {
     // load the signature of the previously sent message and copy it to the protocol context
     unsigned char *last_signature = NULL;
     size_t last_signature_len = 0;
@@ -69,33 +65,32 @@ esp_err_t *ubirch_message(msgpack_sbuffer *sbuf, const unsigned char *uuid, int3
     }
     free(last_signature);
 
+    // create and initialize buffer and packer for msgpack type payload
+    msgpack_sbuffer sbuf; /* buffer */
+    msgpack_packer pk;    /* packer */
+    msgpack_sbuffer_init(&sbuf); /* initialize buffer */
+    msgpack_packer_init(&pk, &sbuf, msgpack_sbuffer_write); /* initialize packer */
+
     // create array[ timestamp, value1, value2 ])
-    msgpack_pack_array(pk, num + 1);
+    msgpack_pack_array(&pk, num + 1);
     struct timeval tv;
     gettimeofday(&tv, NULL);
     uint64_t ts = (uint64_t) tv.tv_sec * (uint64_t) 1000000 + tv.tv_usec;
 
-    msgpack_pack_uint64(pk, ts);
+    msgpack_pack_uint64(&pk, ts);
     for (int i = 0; i < num; ++i) {
-        msgpack_pack_int32(pk, values[i]);
+        msgpack_pack_int32(&pk, values[i]);
     }
 
     // create ubirch protocol message
-    ubirch_protocol_message(upp, proto_chained, UBIRCH_PROTOCOL_TYPE_MSGPACK, sbuf->data, sbuf->size);
+    ubirch_protocol_message(upp, proto_chained, UBIRCH_PROTOCOL_TYPE_MSGPACK, sbuf.data, sbuf.size);
 
     // store signature of the new message
     ubirch_store_signature(upp->signature, UBIRCH_PROTOCOL_SIGN_SIZE);
 
-    msgpack_sbuffer_destroy(sbuf);  // clear data from buffer
-    sbuf->data = upp->data;
-    sbuf->size = upp->size;
-    sbuf->alloc = upp->alloc;
-
-    // free allocated resources
-    msgpack_packer_free(pk);
-    ubirch_protocol_free(upp);
-
-    ESP_LOG_BUFFER_HEXDUMP(TAG, sbuf->data, (uint16_t) sbuf->size, ESP_LOG_DEBUG);
+    ESP_LOG_BUFFER_HEXDUMP(TAG, upp->data, (uint16_t) upp->size, ESP_LOG_DEBUG);
+    ESP_LOGI(TAG, "upp-data: %s", upp->data);
+    ESP_LOGI(TAG, "upp-size: %d", upp->size);
 
     return ESP_OK;
 }
